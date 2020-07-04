aab9c7c3759f14c198f60505e8d4c9497ccab0b1
Григорий Бойко <boiko.gri@gmail.com>
2020-07-04 23:06:55 +0700

    Создана основа

--- RGB/ViewController.swift
+++ RGB/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    
+    @IBOutlet var blueTF: UITextField!
+    @IBOutlet var greenTF: UITextField!
+    @IBOutlet var redTF: UITextField!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    
+    @IBOutlet var rgbView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
