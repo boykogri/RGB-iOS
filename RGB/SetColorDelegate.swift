c9c02a14315696dce6f65c924402d27ed056ff06
Григорий Бойко <boiko.gri@gmail.com>
2020-08-02 12:37:37 +0700

    Создание протокола

---
+++ RGB/SetColorDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  SetColorDelegate.swift
+//  RGB
+//
+//  Created by Григорий Бойко on 02.08.2020.
+//  Copyright © 2020 Test. All rights reserved.
+//
+
+protocol SetColorDelegate {
+    func userChosenColor()
+}
